67e20a5ed288c7677570485af4354b74c2019e36
mlu <melodylu@users.noreply.github.com>
2015-07-14 15:28:25 -0700

    implement ._each

--- src/each-and-filter.js
+++ src/each-and-filter.js
@@ -3,6 +3,15 @@
 
   window._ = {};
 
+// mel test variables below
+// var animals = ['tiger', 'monkey', 'slug', 'kitten', 'parrot'];
+// var animalsObj = {cage1: 'tiger', cage2: 'monkey', cage3: 'slug', cage4:'kitten', cage5:'parrot'};
+
+// var print = function (value, key, collection){
+//   console.log(value);
+// }
+
+
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
@@ -10,8 +19,23 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
-    
+
+    if (Array.isArray(collection)){
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var i in collection){
+        iterator(collection[i], i, collection);
+      }
+    }
   };
+
+
+_.each(animals, print)
+
+_.each(animalsObj, print)
+
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test){
