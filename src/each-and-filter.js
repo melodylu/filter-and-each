6c591ddea99e47e0db31caf1c58cd7eeca92f52c
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-07-14 13:24:13 -0400

    Initialized repository

---
+++ src/each-and-filter.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  window._ = {};
+
+
+  // Call iterator(value, key, collection) for each element of collection.
+  // Accepts both arrays and objects.
+  //
+  // Note: _.each does not have a return value, but rather simply runs the
+  // iterator function over each item in the input collection.
+  _.each = function(collection, iterator) {
+    
+  };
+
+  // Return all elements of an array that pass a truth test.
+  _.filter = function(collection, test){
+    // your code goes here
+  };
+
+}());
